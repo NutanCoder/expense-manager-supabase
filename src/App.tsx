d146a3a6cb8b9763b9d6ca3a5a1ccc49e94a7d79
Nutan Singh <nutansingh159@gmail.com>
2025-06-27 10:54:05 +0530

    error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import HomePage from "./pages/HomePage";
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer />
-    </Provider>
+    <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer />
+      </Provider>
+    </ErrorBoundary>
   );
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,12 +8,34 @@ function HomePage() {
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
+
   return (
-    <div className="flex flex-col">
-      <h4>HomePage</h4>
-      <button onClick={notify}>{t("submit")}</button>
-      <button onClick={changeLanguage.bind(null, "en")}>English</button>
-      <button onClick={changeLanguage.bind(null, "hi")}>Hindi </button>
+    <div className="container mx-auto px-4 py-10">
+      <div className="flex flex-col items-center gap-6 p-8">
+        <h4 className="text-3xl font-bold text-primary">HomePage</h4>
+        <button
+          onClick={notify}
+          className="px-6 py-2 rounded-md shadow text-white bg-blue-600 hover:bg-blue-900 transition"
+        >
+          {t("submit")}
+        </button>
+
+        <div className="flex gap-4">
+          <button
+            onClick={changeLanguage.bind(null, "en")}
+            className="px-4 py-2 border border-gray-300 rounded-md hover:bg-gray-100"
+          >
+            English
+          </button>
+
+          <button
+            onClick={changeLanguage.bind(null, "hi")}
+            className="px-4 py-2 border border-gray-300 rounded-md hover:bg-gray-100"
+          >
+            Hindi
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
